706fbd23a36ed042555daf51f78b1ef6b7bdb062
soni <sonivishwakarma2810@gmail.com>
2022-07-29 12:30:21 +0530

    sass update

--- js/custom.js
+++ js/custom.js
@@ -55,3 +55,21 @@ $(document).ready(function(){
   });
 });
 
+const body = document.body;
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const headerHeight = document.querySelector("header").offsetHeight;
+main.style.top = headerHeight + "px";
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset;
+  if (currentScroll - lastScroll > 0) {
+    header.classList.add("scroll-down");
+    header.classList.remove("scroll-up");
+  } else {
+    // scrolled up -- header show
+    header.classList.add("scroll-up");
+    header.classList.remove("scroll-down");
+  }
+  lastScroll = currentScroll;
+})
